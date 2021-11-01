f4c42cc7af8702231b11f72d8db00660fde42772
Net <talon_hq@outlook.com>
2021-11-01 21:58:52 +0000

    Stage 1 complete, splits dump of classes by class

--- Scripts/S1_SplitByClass.py
+++ Scripts/S1_SplitByClass.py
@@ -1,9 +1,12 @@
 import os
 import re
 
+
 search_str = "// Class Mordhau."
 
-input_CPP = "..\SampleDump\Cpp\SDK\Mordhau_classes.h"
+input_CPP = r"..\SampleDump\Cpp\SDK\Mordhau_classes.h"
+ClassDirectory = r"..\Output"
+print(ClassDirectory + " " + str(os.path.isdir(ClassDirectory)))
 
 if os.path.isfile(input_CPP):
     print(input_CPP + " successfully located")
@@ -12,12 +15,12 @@ if os.path.isfile(input_CPP):
     Lines = file1.readlines()
     LineIndexList = []
     TerminalLine = 0
-
+    FileLines = enumerate(Lines, 1)
     # get line indexes for each class block
-    for num, line in enumerate(Lines, 1):
+    for num, line in FileLines:
         out = line.strip()
         if out.startswith(search_str):
-            print(line + " found at : " + str(num))
+            print(out + " found at : " + str(num))
             LineIndexList.append(num)
         # determine end of file, last instance of "};"
         elif out.startswith("};"):
@@ -25,5 +28,22 @@ if os.path.isfile(input_CPP):
 
 print("TerminalLine = " + str(TerminalLine))
 
-# split file at N1 to N2-1, if n2-1 does not exist use last line of file "};"
-# testing webhook
+# split file at N1 to N2-1, if n2-1 does not exist use TerminalLine"
+for n, v in enumerate(LineIndexList):
+    # get n+1
+    if n + 1 < len(LineIndexList):
+        n2 = LineIndexList[n + 1] - 1
+    else:
+        n2 = TerminalLine
+    # get class title
+    # as this list is enumerated from index 0 as 1
+    FullLine = Lines[v - 1].strip().split(".")
+    ClassFileName = (FullLine[1] + ".h")
+    print(ClassFileName + " " + str(v) + " and " + str(n2))
+    ClassContents = Lines[(v - 1):(n2)]
+    ClassFile = ClassDirectory + "\\" + ClassFileName
+    ClassFile = ClassFile.strip().replace("\\", "/")
+    print(ClassFile)
+    with open(ClassFile, 'w') as f:
+        for item in ClassContents:
+            f.write(item + "\n")
